d61a8f8a6ea7f97204c596d1bbf0dcda6e1bcf57
gyro永不抽风 <1247006353@qq.com>
2022-01-17 23:48:12 +0800

    feat: add avalonia project

---
+++ MahiruLauncher/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MahiruLauncher.Manager;
+using MahiruLauncher.Mvvm;
+using ReactiveUI;
+
+namespace MahiruLauncher.ViewModels
+{
+    public class MainViewModel : NotifyObject
+    {
+        public ScriptManager ScriptManager => ScriptManager.GetInstance();
+        
+        public ScriptTaskManager ScriptTaskManager => ScriptTaskManager.GetInstance();
+    }
+}
---
+++ MahiruLauncher/ViewModels/NewScriptViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MahiruLauncher.Mvvm;
+
+namespace MahiruLauncher.ViewModels
+{
+    public class NewScriptViewModel : NotifyObject
+    {
+        private string _name = "";
+
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+        
+        private string _description = "";
+        
+        public string Description
+        {
+            get => _description;
+            set => SetProperty(ref _description, value);
+        }
+
+        private string _identifier = "";
+        
+        public string Identifier
+        {
+            get => _identifier;
+            set => SetProperty(ref _identifier, value);
+        }
+    }
+}
