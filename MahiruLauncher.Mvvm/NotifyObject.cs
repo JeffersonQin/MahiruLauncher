9049e03f9ac65d7256d4f8ab67497b899e7f958c
gyro永不抽风 <1247006353@qq.com>
2022-01-17 23:47:07 +0800

    feat: add mvvm base classes

---
+++ MahiruLauncher.Mvvm/NotifyObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MahiruLauncher.Mvvm
+{
+    public partial class NotifyObject : INotifyPropertyChanged
+    {
+        public bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action beforeChanged = null,
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+            beforeChanged?.Invoke();
+            backingStore = value;
+            onChanged?.Invoke();
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
